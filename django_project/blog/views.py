4cefb0a71e893b9ef72955360b4598a6904662c3
Alleck101 <sasha.belous1@gmail.com>
2019-03-19 20:07:55 +0200

    Fix

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+	{
+		'author': 'Alleck',
+		'title': 'Blog Post 1',
+		'content': 'First post here, lads!',
+		'date_posted': 'March 19, 2019'
+	},
+	{
+		'author': 'G.Rapefruit',
+		'title': 'Blog Post 2',
+		'content': 'Second post here, lads!',
+		'date_posted': 'March 20, 2019'
+	}
+]
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 	
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html')
