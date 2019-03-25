a9ffdb639ad92c92babdef14e0598ba3ea178cbd
Alleck101 <sasha.belous1@gmail.com>
2019-03-25 21:57:23 +0200

    Redefined save() method to crop images

--- django_project/users/models.py
+++ django_project/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Profile(models.Model):
@@ -8,6 +9,26 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f'{self.user.username} Profile'
+
+
+	"""
+	↓ Save() redefinition.
+	I redefined save method in user app, so that it will resize large images into smaller ones.
+	If image H or W is greater than 300, it will be resized upon saving.
+
+	PROBLEM SOLVED -> images stored on drive will be smaller and website will load faster.
+	"""
+
+	#TODO: Delete old images after updating to new
+	def save(self):
+		super().save()
+
+		img = Image.open(self.image.path)
+
+		if img.height > 300 or img.width > 300:
+			output_size = (300, 300)
+			img.thumbnail(output_size)
+			img.save(self.image.path)
 
 
 """
